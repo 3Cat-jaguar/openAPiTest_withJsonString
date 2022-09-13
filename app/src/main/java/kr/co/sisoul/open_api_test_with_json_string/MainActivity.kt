47d46c636ba4d52c6178ffb4ae2d0ffafd97b6f3
YuriLee <yuri.lee@sisoul.co.kr>
2022-09-13 14:18:56 +0900

    first commit

---
+++ app/src/main/java/kr/co/sisoul/open_api_test_with_json_string/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.sisoul.open_api_test_with_json_string
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
